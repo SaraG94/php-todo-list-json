218466eb89c1932851b413c0c1400e76e7f1fda8
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-22 16:28:08 +0100

    FEAT bootsrap

--- index.php
+++ index.php
@@ -6,19 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
    
-
+    <!-- dentro app sintassi di vue -->
     <div id="app">
         <section>
             <div class="container">
                 <h1>{{ title }}</h1>
-
-                <ul>
-                    <li v-for="todo in todolists" :key="todo.id">{{todo}}</li>
+            
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="todo in todolists" :key="todo.id">{{todo}}</li>
                 </ul>
+
             </div>
 
             
@@ -26,5 +28,6 @@
     </div>
 
     <script src="./app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
