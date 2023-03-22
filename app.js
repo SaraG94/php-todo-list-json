8874b0b52125216a561271aa9107f029dfd56320
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-22 17:12:18 +0100

    FEAT axios.post in saveTask

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ createApp({
     methods:{
         saveTask(){
             console.log('invio',this.newElToDo);
+
+            $data_list ={
+                todo:this.newElToDo
+            }
+            //chiamata con post:indico percorso, indico oggetto che passo, configuro l'oggetto come se provenisse da un form
+            axios.post('./server.php', $data_list,{
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
         },
         createToDoList(){
             axios
