1dc269c461d98ad3ec201306588ccfe8878c0862
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-22 17:39:29 +0100

    FEAT take call of SaveTask+feat json_encode

--- server.php
+++ server.php
@@ -5,5 +5,16 @@ $todolist_string = file_get_contents('./todolist.json');
 //impostare content-type json
 header('Content-Type: application/json');
 
-echo $todolist_string;
+$todo_list =json_decode($todolist_string, true);
+
+//recupero le chiamata che arriva con saveTask
+$newElToDo = isset($_POST['todo']) ? $_POST['todo'] : null;
+
+//se esiste lo aggiungo a $todo_list 
+if($newElToDo !== null){
+    $todo_list[] = $newElToDo;
+}
+
+//e salvo il risultato
+echo json_encode($todolist_string); 
 ?>
